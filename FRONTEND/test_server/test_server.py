c489bd30895832e164bde0d2d10e9f4f98ebabfe
abhishek kanthed <bestkanthed@gmail.com>
2017-05-09 11:48:06 +0530

    version one with sass. Ready to deploy

--- FRONTEND/test_server/test_server.py
+++ FRONTEND/test_server/test_server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('hello.html')
+   return render_template('index.html')
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css',path)
 
 if __name__ == '__main__':
    app.run(debug = True)
